6fd25e5045ad305c4b960e5c5273e30c5da53a49
JAAM <joelandujo@gmail.com>
2021-05-17 12:24:08 -0700

    1Era parte 17May/2021

---
+++ ULSAApp/ULSAApp/ULSAApp.Skia.Tizen/Program.cs
@@ -0,0 +1,14 @@
+﻿using Tizen.Applications;
+using Uno.UI.Runtime.Skia;
+
+namespace ULSAApp.Skia.Tizen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new TizenHost(() => new ULSAApp.App(), args);
+            host.Run();
+        }
+    }
+}
